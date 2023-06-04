7a08b7b569f0edf45c42506601fce056ab7c556d
michaelmirhom <micharby73@gmail.com>
2023-06-03 23:49:20 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+    function handleDarkModeClick() {
+        setIsDarkMode((isDarkMode) => !isDarkMode);
+    }
 
-  return (
-    <div className={appClass}>
-      <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
-      </header>
-      <ShoppingList items={itemData} />
-    </div>
-  );
+    return (
+        <div className={isDarkMode ? "App dark" : "App light"}>
+            <header>
+                <h2>Shopster</h2>
+                <button onClick={handleDarkModeClick}>
+                    {isDarkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+                </button>
+            </header>
+            <ShoppingList items={itemData} />
+        </div>
+    );
 }
 
 export default App;
+
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
+    const [isInCart, setIsInCart] = useState(false);
+
+    function handleAddToCartClick() {
+        setIsInCart((isInCart) => !isInCart);
+    }
+
+    return (
+        <li className={isInCart ? "in-cart" : ""}>
+            <span>{name}</span>
+            <span className="category">{category}</span>
+            <button className="add" onClick={handleAddToCartClick}>
+                {isInCart ? "Remove From Cart" : "Add to Cart"}
+            </button>
+        </li>
+    );
 }
 
 export default Item;
